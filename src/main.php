a4528ec5298fc9722f805cbcb43123afccd3e7c3
Sami <samiys005@gmail.com>
2024-07-16 17:16:24 +0200

    created main.php as the entry point to parse XML

---
+++ src/main.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\Database\DatabaseFactory;
+use App\Repository\ItemRepository;
+use App\Parser\XMLParser;
+use App\DataHandler\DataInserter;
+use App\Logger\ErrorLogger;
+
+try {
+    $database = DatabaseFactory::create();
+    $pdo = $database->getConnection();
+
+    $xmlFile = 'feed.xml';
+    $xml = XMLParser::parse($xmlFile);
+
+    if ($xml) {
+        $itemRepository = new ItemRepository($pdo);
+        $dataInserter = new DataInserter($itemRepository);
+        $dataInserter->insertData($xml);
+    }
+    echo "Data processing complete.\n";
+} catch (Exception $e) {
+    ErrorLogger::logError($e->getMessage());
+    echo "An error occurred. Check the error log for details.\n";
+}
+?>
+
