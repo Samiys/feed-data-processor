6c2817ba89d04dfbc07beac319df95083de642a7
Sami <samiys005@gmail.com>
2024-07-16 17:05:03 +0200

    adding Error Logger

---
+++ src/Logger/ErrorLogger.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Logger;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+class ErrorLogger {
+    private static $logger;
+
+    private static function getLogger(): Logger
+    {
+        if (!self::$logger) {
+            self::$logger = new Logger('app_logger');
+            self::$logger->pushHandler(new StreamHandler(__DIR__ . '/../../logs/app.log', Logger::ERROR));
+        }
+        return self::$logger;
+    }
+
+    public static function logError($message, $context = []): void
+    {
+        self::getLogger()->error($message, $context);
+    }
+}
+?>
