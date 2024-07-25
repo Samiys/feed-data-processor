c04106100b9e035c765fce3e5ec9fc82cc6375e0
Sami <samiys005@gmail.com>
2024-07-26 00:23:22 +0200

    Refactor DatabaseFactory for dependency injection

--- src/Database/DatabaseFactory.php
+++ src/Database/DatabaseFactory.php
@@ -2,17 +2,24 @@
 namespace App\Database;
 
 use App\Exceptions\DatabaseConnectionException;
+use App\Config\DatabaseConfig;
 
 class DatabaseFactory {
+    private $config;
+
+    public function __construct(DatabaseDTO $config) {
+        $this->config = $config;
+    }
+
     /**
      * @throws DatabaseConnectionException
      */
-    public static function create(string $type = 'mysql'): DatabaseInterface {
+    public function create(string $type = 'mysql'): DatabaseInterface {
         switch ($type) {
             case 'mysql':
-                return new MySQLDatabase();
+                return new MySQLDatabase($this->config->getMySQLConfig());
             case 'pgsql':
-                return new PostgreSQLDatabase();
+                return new PostgreSQLDatabase($this->config->getPgSQLConfig());
             default:
                 throw new DatabaseConnectionException("Unsupported database type: $type");
         }
