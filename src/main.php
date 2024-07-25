3954c7073e1aa56fe086394bd55798b8f82d80b5
Sami <samiys005@gmail.com>
2024-07-26 01:33:10 +0200

    Integrate parsers in main script

--- src/main.php
+++ src/main.php
@@ -2,25 +2,37 @@
 
 require_once 'vendor/autoload.php';
 
+use App\Database\DatabaseDTO;
 use App\Database\DatabaseFactory;
+use App\Parser\JSONParser;
+use App\Parser\XMLStreamParser;
 use App\Repository\ItemRepository;
-use App\Parser\XMLParser;
 use App\DataHandler\DataInserter;
 use App\Logger\ErrorLogger;
 
+
 try {
     $databaseType = $argv[1];
-    $database = DatabaseFactory::create($databaseType);
+    $config = new DatabaseDTO(require __DIR__ . '/../../config/config.php');
+    $databaseFactory = new DatabaseFactory($config);
+    $database = $databaseFactory->create($databaseType);
     $pdo = $database->getConnection();
 
-    $xmlFile = 'feed.xml';
-    $xml = XMLParser::parse($xmlFile);
+    $file = 'feed.xml';
+    $itemRepository = new ItemRepository($pdo);
+    $dataInserter = new DataInserter($itemRepository);
 
-    if ($xml) {
-        $itemRepository = new ItemRepository($pdo);
-        $dataInserter = new DataInserter($itemRepository);
-        $dataInserter->insertData($xml);
+    if (pathinfo($file, PATHINFO_EXTENSION) === 'xml') {
+        XMLStreamParser::parse($file, function ($item) use ($dataInserter) {
+            $dataInserter->insertData([$item]);
+        });
+    } elseif (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
+        $data = JSONParser::parse($file);
+        if ($data) {
+            $dataInserter->insertData($data);
+        }
     }
+
     echo "Data processing complete.\n";
 } catch (Exception $e) {
     ErrorLogger::logError($e->getMessage());
